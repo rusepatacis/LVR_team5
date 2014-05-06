7de3d01c4e667ff0bcfbab52e871581faa1cca70
Jaka Demsar <jaka.demsar0@gmail.com>
2014-05-06 18:29:17 +0200

    Olepsava prevedbe barvanja grafov na SAT.

--- team5_sat_solver/prevedba_coloring.py
+++ team5_sat_solver/prevedba_coloring.py
@@ -5,6 +5,7 @@ from operands import *
 
 """ Prevedba problema barvanja grafa na problem SAT """
 
+
 def barvanje(G, b):
     """
         Funkcija, ki vhodni graf G prevede na problem SAT (v obliki logicne formule).
@@ -24,31 +25,17 @@ def barvanje(G, b):
     # Za lazje delo vozlisca shranimo v slovar oblike (ime_vozlisca, barva) -> pripadajoca spremenljivka.
     spremenljivke = dict(((v, k), V('%s%d' % (v, k))) for v in vozlisca for k in barve)
 
-    # Vsako vozlišče je pobarvano z vsaj eno barvno.
-    vsaj_ena_barva = []
-    for v in vozlisca:
-        tmp = []
-        for k in barve:
-            tmp.append(spremenljivke[(v,k)])
-        vsaj_ena_barva.append(Or(tmp))
-    vsaj_ena_barva = And(vsaj_ena_barva)
-
+    # Vsako vozlišče je pobarvano z vsaj eno barvo.
+    vsaj_ena_barva = And([Or([spremenljivke[(v, k)] for k in barve])for v in vozlisca])
 
     # Nobeno vozlišče ni hkrati pobarvnano z dvema barvama.
     pari_barv = [(barve[j], barve[i]) for i in range(len(barve)) for j in range(i)]
     nobeno_dvakrat = And([And([Not(And([spremenljivke[(v, par_barv[0])], spremenljivke[(v, par_barv[1])]]))
                                for par_barv in pari_barv]) for v in vozlisca])
 
-
     # Povezani vozlišči nista iste barve.
-    povezani_nista_iste = []
-    for (vi,vj) in G:
-        tmp = []
-        for k in barve:
-            tmp.append(Or([Not(spremenljivke[(vi,k)]), Not(spremenljivke[(vj,k)])]))
-        povezani_nista_iste.append(And(tmp))
-    povezani_nista_iste = And(povezani_nista_iste)
-
+    povezani_nista_iste = And([And([Or([Not(spremenljivke[(vi, k)]), Not(spremenljivke[(vj, k)])]) for k in barve])
+                               for (vi, vj) in G])
 
     # Prevedba je konjunkcija pogojev.
     return And([vsaj_ena_barva, nobeno_dvakrat, povezani_nista_iste])
